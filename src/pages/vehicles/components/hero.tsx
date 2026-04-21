f9ce2556380ed70fa6fafad457855fcf90a79b7e
Ahmed Ibrahim <ahmed.ibrahim.elsayed01@gmail.com>
2026-04-22 00:19:53 +0200

    feat: add hero of vehicles page

---
+++ src/pages/vehicles/components/hero.tsx
@@ -0,0 +1,31 @@
+import drone from "@/assets/images/drone.png";
+import Marquee from "@/components/common/marquee";
+
+function Hero() {
+  return (
+    <section>
+      <div className="h-dvh flex flex-col pt-20 pb-5 justify-between">
+        <div className="text-3xl section-inline-padding">
+          Building the future of <span className="text-brand">drones</span>{" "}
+          technology
+        </div>
+        <div className="my-5 relative isolate">
+          <figure>
+            <img src={drone} alt="Drone image" />
+          </figure>
+          <Marquee text="drone" />
+        </div>
+        <div className="flex flex-wrap gap-x-1 section-inline-padding text-xs justify-between *:uppercase">
+          <span>Safer</span>
+          <span>Sustainable</span>
+          <span>More reliable</span>
+          <span>Longer lasting</span>
+          <span>Faster charging</span>
+          <span>Smaller</span>
+          <span>Lighter</span>
+        </div>
+      </div>
+    </section>
+  );
+}
+export default Hero;
