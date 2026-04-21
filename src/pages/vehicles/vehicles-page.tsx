3c82e7d1f43a2a2d6479610de04764eb9c5c42fd
Ahmed Ibrahim <ahmed.ibrahim.elsayed01@gmail.com>
2026-04-22 00:20:09 +0200

    feat: add vehicles page

---
+++ src/pages/vehicles/vehicles-page.tsx
@@ -0,0 +1,15 @@
+import Navbar from "../../components/layout/navbar";
+import Hero from "./components/hero";
+
+function VehiclesPage() {
+  return (
+    <div>
+      <Navbar />
+      <main>
+        <Hero />
+      </main>
+    </div>
+  );
+}
+
+export default VehiclesPage;
